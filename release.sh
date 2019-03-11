49e3e3ec4bb7bd1e1f60a2b825daa969fd54a65b
yuanbo <yuanbosdu@163.com>
2019-03-10 20:50:41 -0700

    modify release.sh

--- release.sh
+++ release.sh
@@ -2,14 +2,21 @@
 if [[ -f ../release.py ]]; then
 	rm -rf ../release.py
 fi
-cp release.py ../release.py
-cd ..
-python3 release.pyc
 
-#find -name '*.pyc' -exec rename 's/.cpython-35//' {} \;
-#find -name '*.pyc' -execdir mv {} .. \;
-find -name '*.py' -type f -print -exec rm {} \;
-#find -name '__pycache__' -exec rmdir {} \;
+cp release.py ../release.py
+
+cd ..
+if [[ -d ./monit_backup ]]; then
+	rm -rf ./monit_backup
+fi
+
+cp -rf monit monit_backup
+python3 release.py
+
+find ./monit_backup -name '*.pyc' -exec rename 's/.cpython-35//' {} \;
+find ./monit_backup -name '*.pyc' -execdir mv {} .. \;
+find ./monit_backup -name '*.py' -print -exec rm {} \;
+find ./monit_backup -name '__pycache__' -exec rmdir {} \;
 
 echo "release done!"
 exit 0
