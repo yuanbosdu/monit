55119d4cdd83cf63bec40ca9bb0385f5d875ee16
yuanbo <yuanbosdu@163.com>
2019-04-02 22:35:53 +0800

    modify blog

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Blog
 # Register your models here.
+
+
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Blog, BlogAdmin)
--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+# blog model
+class Blog(models.Model):
+
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    see = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "%s" % self.title
