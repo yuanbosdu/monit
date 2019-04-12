6d707c0b92ef989625eec900490c7aea6be3cb9a
boyuan <yuanbosdu@163.com>
2019-04-12 16:28:46 +0800

    modify

--- client/mqtt_server.py
+++ client/mqtt_server.py
@@ -20,6 +20,38 @@ DATABASE = '/opt/monit/db.sqlite3'
 
 global sclient
 
+# connect to the database
+db = SqliteDatabase(database=DATABASE)
+db.connect()
+
+class BaseModel(Model):
+
+    class Meta:
+        database = db
+
+class Device(BaseModel):
+    name = CharField(max_length=50)
+    desc = TextField()
+    address = CharField(max_length=50)
+    ctime = DateTimeField()
+    mtime = DateTimeField()
+    ename = CharField(max_length=50)
+    uuid = UUIDField()
+
+    class Meta:
+        table_name='zigbee_zigbee'
+
+
+class DeviceState(BaseModel):
+    utime = DateTimeField()
+    state = CharField(max_length=50)
+    zigbee_id = IntegerField()
+    state_type = CharField(max_length=50)
+
+    class Meta:
+        table_name='zigbee_zigbeestate'
+
+
 def on_connect(client, userdata, flags, rc):
     print("Connect with result code %s" % str(rc))
     client.subscribe('mqtt_data')
@@ -27,6 +59,17 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(client)
     print(msg.topic, str(msg.payload))
+    payload = str(msg.payload).split(';')
+    muuid = payload[-1]
+    muuid = muuid[5:-1]
+    print(muuid)
+    mDevice = Device.filter(uuid=muuid)
+    for md in mDevice:
+        print(md)
+    if len(mDevice) > 0:
+        print(mDevice[0])
+    else:
+        print("no device found")
 
 def start_server_client():
     sclient = mqtt.Client("server_client")
